bbd1dca6d911fb6572a9dd189b10065ecd6bb4f5
iFgR <ifgr.fagner@gmail.com>
2018-06-24 22:04:42 -0300

    Build last version

--- spec/emReal.spec.js
+++ spec/emReal.spec.js
@@ -2,6 +2,7 @@ const emReal = require("../src/emReal");
 
 describe("`emReal`", () => {
   it("deve escrever valores em reais", () => {
+    expect(emReal("10.529,38")).toBe("dez mil quinhentos e vinte e nove reais e trinta e oito centavos");
     expect(emReal("10")).toBe("dez reais");
     expect(emReal("10,25")).toBe("dez reais e vinte e cinco centavos");
     expect(emReal("0,25")).toBe("vinte e cinco centavos");
